62d0389f32ee7cbb31ed29b987c2976c13115762
bizcyben <Benchaphon500103@gmail.com>
2024-06-23 11:18:08 +0700

    start

---
+++ DiaryProject/DiaryProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiaryProject
+//
+//  Created by Benchaphon Phukhen on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiaryProject/DiaryProject/DiaryProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryProjectApp.swift
+//  DiaryProject
+//
+//  Created by Benchaphon Phukhen on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DiaryProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
